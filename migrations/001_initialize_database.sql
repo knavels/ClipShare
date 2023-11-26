8b83db9190e178c0f55dc09145f66ede8d098e77
Navid Dezashibi <dezashibi@gmail.com>
2023-11-26 16:01:06 +0400

    fix migration error

--- migrations/001_initialize_database.sql
+++ migrations/001_initialize_database.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS clip_share
 (
-    id              TEXT PRIMARY KEY NOT NULL,
-    short_code      TEXT UNIQUE NOT NULL,
-    content         TEXT NOT NULL,
-    title           TEXT,
-    created_at      DATETIME NOT NULL,
-    expires_at      DATETIME,
-    password        TEXT,
-    views           BIGINT NOT NULL,
+    id          TEXT PRIMARY KEY NOT NULL,
+    short_code  TEXT UNIQUE NOT NULL,
+    content     TEXT NOT NULL,
+    title       TEXT,
+    created_at  DATETIME NOT NULL,
+    expires_at  DATETIME,
+    password    TEXT,
+    views       BIGINT NOT NULL
 );
